007090c2f154c1c2f43c72703427762b57666074
Tmeng <tonymengt@gmail.com>
2022-09-01 12:47:09 -0400

    removed comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,27 +1,8 @@
 const fs = require("fs");
-// // TODO: Create a function that returns a license badge based on which license is passed in
-// // If there is no license, return an empty string
-// function renderLicenseBadge(license) {}
-
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {}
-
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
-
-// // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   return `# ${data.title}
-
-// `;
-// }
-// module.exports = generateMarkdown;
 
 const writeFile = fileContent => {
   return new Promise((resolve, reject) => {
-    fs.writeFile('./README.md', fileContent, err => {
+    fs.writeFile('./README_test.md', fileContent, err => {
       if (err) {
         reject(err);
         return;
